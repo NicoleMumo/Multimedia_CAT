d4d97a114c24ffb34fa986c1737acb291d7730d9
NicoleMumo <156808689+NicoleMumo@users.noreply.github.com>
2026-06-26 15:38:33 +0300

    Change video element ID and update toggle button text

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function toggleVideo() {
-    const video = document.getElementById("video");
+    const video = document.getElementById("wildlifeVideo");
+    const button = document.getElementById("videoToggleBtn");
 
     if (video.style.display === "none") {
         video.style.display = "block";
         video.play();
+        button.textContent = "Pause Video";
     } else if (video.paused) {
         video.play();
+        button.textContent = "Pause Video";
     } else {
+        video.pause();
         video.style.display = "none";
+        button.textContent = "Play Video";
     }
 }
